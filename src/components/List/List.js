6153830337f1ede798cdad0bc96c919d3ff1569d
Jacqueline Nguyen <jnguyen9763@gmail.com>
2020-07-12 19:00:33 -0400

    added checking and deleting functionality to lists

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,25 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import styles from './List.module.css';
+import ListItem from './ListItem/ListItem';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function List() {
 	const [input, setInput] = useState('');
 	const [items, setItems] = useState([]);
+	const [toggleScroll, setToggleScroll] = useState(false);
 
 	useEffect(() => {
-		// scroll to bottom
+		// scroll to bottom when a new item gets added
 		const scrollWindow = document.querySelector('#list');
 		scrollWindow.scrollTop = scrollWindow.scrollHeight - scrollWindow.clientHeight;
-	}, [items])
+	}, [toggleScroll]);
 
 	const updateList = e => {
 		e.preventDefault();
-		setItems([...items, input]);
+		if (input === '') return;
+		const listItem = {
+			input,
+			id: uuidv4(),
+			isChecked: false,
+		}
+		setItems([...items, listItem]);
+		setToggleScroll(!toggleScroll);
 		setInput('');
 	}
 
+	const checkListItem = id => {
+		const itemsCopy = [...items];
+		for (let item of itemsCopy) {
+			if (item.id !== id) continue;
+			item.isChecked = !item.isChecked;
+			setItems(itemsCopy);
+			break;
+		}
+	}
+
+	const deleteListItem = id => {
+		const itemsCopy = [...items];
+		for (let i = 0; i < itemsCopy.length; i++) {
+			if (itemsCopy[i].id !== id) continue;
+			itemsCopy.splice(i, 1);
+			setItems(itemsCopy);
+			break;
+		}
+
+	}
+
 	const displayListItems = () => {
-		// TODO: add unique key
-		return items.map(item => <div className={styles.listItem}>{item}</div>);
+		return items.map(({ id, input, isChecked }) =>
+			<ListItem
+				id={id}
+				input={input}
+				isChecked={isChecked}
+				key={id}
+				checkListItem={checkListItem}
+				deleteListItem={deleteListItem}
+			/>);
 	}
 
 	return (
---
+++ src/components/List/ListItem/ListItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './ListItem.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle, faCircle } from '@fortawesome/free-regular-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+
+export default function ListItem({ id, input, isChecked, checkListItem, deleteListItem }) {
+	return (
+		<div className={styles.ListItem}>
+			<button className={styles.checkBox} onClick={() => checkListItem(id)}>
+				{isChecked ?
+					<FontAwesomeIcon icon={faCheckCircle} /> :
+					<FontAwesomeIcon icon={faCircle} />
+				}
+			</button>
+			<div className={styles.input}>{input}</div>
+			{isChecked &&
+				<button className={styles.delete} onClick={() => deleteListItem(id)}>
+					<FontAwesomeIcon icon={faTrash} />
+				</button>
+			}
+		</div>
+	);
+}
