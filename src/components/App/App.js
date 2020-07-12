4287fefa5435db60d5e8128fa70de10e2ddd1c17
Jacqueline Nguyen <jnguyen9763@gmail.com>
2020-07-12 01:09:31 -0400

    added calendar component

---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import Calendar from '../Calendar/Calendar'
+
+export default function App() {
+  return (
+    <Calendar />
+  );
+}
---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Calendar.module.css'
+
+export default function Calendar() {
+	return (
+		<div>
+			test
+		</div>
+	);
+}
