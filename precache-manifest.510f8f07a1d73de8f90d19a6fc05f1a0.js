4c72e366b8cba6dc2cf4cdcd28fc1ac6f42fbf49
Jacqueline Nguyen <jnguyen9763@gmail.com>
2020-07-13 05:48:14 -0400

    Updates

---
+++ precache-manifest.510f8f07a1d73de8f90d19a6fc05f1a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b889dbdd4e70e6a6a08205df5c5912",
+    "url": "/notetaker/index.html"
+  },
+  {
+    "revision": "622547fc65cc247148b2",
+    "url": "/notetaker/static/css/main.5a256534.chunk.css"
+  },
+  {
+    "revision": "e20ba4bb4c827646a6e0",
+    "url": "/notetaker/static/js/2.9920ede3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/notetaker/static/js/2.9920ede3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "622547fc65cc247148b2",
+    "url": "/notetaker/static/js/main.28aa9839.chunk.js"
+  },
+  {
+    "revision": "e636990943f2b6173c60",
+    "url": "/notetaker/static/js/runtime-main.94ec0166.js"
+  }
+]);
